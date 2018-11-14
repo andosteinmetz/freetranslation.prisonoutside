9ab54adfc5e77a1d5589c7ebf3a52274ebdff569
Andrew Steinmetz <ando.steinmetz@gmail.com>
2018-11-14 01:09:42 -0500

    Enable video embeds.

--- index.php
+++ index.php
@@ -20,8 +20,15 @@
             <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
             <?php $id = get_the_ID(); ?>
             <div class="details__artwork js-artwork-detail">
-                <?php the_post_thumbnail('large'); ?>
-                <!-- <img data-src="<?php the_post_thumbnail_url($id) ?>"> -->
+                <?php if(get_post_meta($id, 'video_embed')) : ?>
+                    <div class="videoWrapper">
+                        <!-- Copy & Pasted from YouTube -->
+                        <!-- <iframe width="560" height="349" src="http://www.youtube.com/embed/n_dZNLr2cME?rel=0&hd=1" frameborder="0" allowfullscreen></iframe> -->
+                        <?php echo get_post_meta($id, 'video_embed')[0]; ?>
+                    </div>
+                <?php else : ?>
+                    <?php the_post_thumbnail('large'); ?>
+                <?php endif; ?>
                 <div class="details__artwork-info">
                     <?php echo get_post_meta($id, 'artist', true); ?> - 
                     <em><!-- <?php echo get_post_meta($id, 'title', true); ?> --><?php the_title(); ?></em>, 
